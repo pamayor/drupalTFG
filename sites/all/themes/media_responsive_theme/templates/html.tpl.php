6bd7ccb3b62d395fb52337765d0a043af9bc5f90
root <pamayor@unirioja.es>
2016-03-23 19:07:47 +0100

    Segundo commit en la rama segundo

---
+++ sites/all/themes/media_responsive_theme/templates/html.tpl.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<head>
+<?php print $head; ?>
+<title><?php print $head_title; ?></title>
+<?php print $styles; ?>
+<?php print $scripts; ?>
+<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+<body class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <div id="fb-root"></div>
+<script>
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v2.5";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+</script>
+  <div id="page">
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+  </div>
+</body>
+</html>
